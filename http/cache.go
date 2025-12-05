c81cc802441b9273c4ed706b4cf793eebd36cffd
yusing <yusing.wys@gmail.com>
2025-12-05 15:55:02 +0800

    feat(http): moved cache data and response modifier here

---
+++ http/cache.go
@@ -0,0 +1,113 @@
+package httputils
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+// Cache is a map of cached values for a request.
+// It prevents the same value from being parsed multiple times.
+type (
+	Cache             map[string]any
+	UpdateFunc[T any] func(T) T
+
+	Credentials struct {
+		Username string
+		Password []byte
+	}
+)
+
+const (
+	cacheKeyQueries   = "queries"
+	cacheKeyCookies   = "cookies"
+	cacheKeyRemoteIP  = "remote_ip"
+	cacheKeyBasicAuth = "basic_auth"
+)
+
+var cachePool = sync.Pool{
+	New: func() any {
+		return make(Cache)
+	},
+}
+
+// NewCache returns a new Cached.
+func NewCache() Cache {
+	return cachePool.Get().(Cache)
+}
+
+// Release clear the contents of the Cached and returns it to the pool.
+func (c Cache) Release() {
+	clear(c)
+	cachePool.Put(c)
+}
+
+// GetQueries returns the queries.
+// If r does not have queries, an empty map is returned.
+func (c Cache) GetQueries(r *http.Request) url.Values {
+	v, ok := c[cacheKeyQueries]
+	if !ok {
+		v = r.URL.Query()
+		c[cacheKeyQueries] = v
+	}
+	return v.(url.Values)
+}
+
+func (c Cache) UpdateQueries(r *http.Request, update func(url.Values)) {
+	queries := c.GetQueries(r)
+	update(queries)
+	r.URL.RawQuery = queries.Encode()
+}
+
+// GetCookies returns the cookies.
+// If r does not have cookies, an empty slice is returned.
+func (c Cache) GetCookies(r *http.Request) []*http.Cookie {
+	v, ok := c[cacheKeyCookies]
+	if !ok {
+		v = r.Cookies()
+		c[cacheKeyCookies] = v
+	}
+	return v.([]*http.Cookie)
+}
+
+func (c Cache) UpdateCookies(r *http.Request, update UpdateFunc[[]*http.Cookie]) {
+	cookies := update(c.GetCookies(r))
+	c[cacheKeyCookies] = cookies
+	r.Header.Del("Cookie")
+	for _, cookie := range cookies {
+		r.AddCookie(cookie)
+	}
+}
+
+// GetRemoteIP returns the remote ip address.
+// If r.RemoteAddr is not a valid ip address, nil is returned.
+func (c Cache) GetRemoteIP(r *http.Request) net.IP {
+	v, ok := c[cacheKeyRemoteIP]
+	if !ok {
+		host, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			host = r.RemoteAddr
+		}
+		v = net.ParseIP(host)
+		c[cacheKeyRemoteIP] = v
+	}
+	return v.(net.IP)
+}
+
+// GetBasicAuth returns *Credentials the basic auth username and password.
+// If r does not have basic auth, nil is returned.
+func (c Cache) GetBasicAuth(r *http.Request) *Credentials {
+	v, ok := c[cacheKeyBasicAuth]
+	if !ok {
+		u, p, ok := r.BasicAuth()
+		if ok {
+			v = &Credentials{u, []byte(p)}
+			c[cacheKeyBasicAuth] = v
+		} else {
+			c[cacheKeyBasicAuth] = nil
+			return nil
+		}
+	}
+	return v.(*Credentials)
+}
---
+++ http/response_modifier.go
@@ -0,0 +1,267 @@
+package httputils
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/rs/zerolog/log"
+	gperr "github.com/yusing/goutils/errs"
+	"github.com/yusing/goutils/synk"
+)
+
+type ResponseModifier struct {
+	bufPool synk.UnsizedBytesPool
+
+	w          http.ResponseWriter
+	buf        *bytes.Buffer
+	statusCode int
+	shared     Cache
+
+	origContentLength int64 // from http.Response in ResponseAsRW, -1 if not set
+	bodyModified      bool
+
+	hijacked bool
+
+	errs gperr.Builder
+}
+
+type Response struct {
+	StatusCode int
+	Header     http.Header
+}
+
+func unwrapResponseModifier(w http.ResponseWriter) *ResponseModifier {
+	for {
+		switch ww := w.(type) {
+		case *ResponseModifier:
+			return ww
+		case interface{ Unwrap() http.ResponseWriter }:
+			w = ww.Unwrap()
+		default:
+			return nil
+		}
+	}
+}
+
+type responseAsRW struct {
+	resp *http.Response
+}
+
+func (r responseAsRW) WriteHeader(code int) {
+	log.Error().Msg("write header after response has been created")
+}
+
+func (r responseAsRW) Write(b []byte) (int, error) {
+	return 0, io.ErrClosedPipe
+}
+
+func (r responseAsRW) Header() http.Header {
+	return r.resp.Header
+}
+
+func ResponseAsRW(resp *http.Response) *ResponseModifier {
+	return &ResponseModifier{
+		statusCode:        resp.StatusCode,
+		w:                 responseAsRW{resp},
+		origContentLength: resp.ContentLength,
+	}
+}
+
+// GetInitResponseModifier returns the response modifier for the given response writer.
+// If the response writer is already wrapped, it will return the wrapped response modifier.
+// Otherwise, it will return a new response modifier.
+func GetInitResponseModifier(w http.ResponseWriter) *ResponseModifier {
+	if rm := unwrapResponseModifier(w); rm != nil {
+		return rm
+	}
+	return NewResponseModifier(w)
+}
+
+// GetSharedData returns the shared data for the given response writer.
+// It will initialize the shared data if not initialized.
+func GetSharedData(w http.ResponseWriter) Cache {
+	if rm := unwrapResponseModifier(w); rm != nil {
+		if rm.shared == nil {
+			rm.shared = NewCache()
+		}
+		return rm.shared
+	}
+	// it's not a response modifier, so return an empty cache
+	return Cache{}
+}
+
+// NewResponseModifier returns a new response modifier for the given response writer.
+//
+// It should only be called once, at the very beginning of the request.
+func NewResponseModifier(w http.ResponseWriter) *ResponseModifier {
+	return &ResponseModifier{
+		bufPool:           synk.GetUnsizedBytesPool(),
+		w:                 w,
+		origContentLength: -1,
+	}
+}
+
+func (rm *ResponseModifier) BufPool() synk.UnsizedBytesPool {
+	return rm.bufPool
+}
+
+// func (rm *ResponseModifier) Unwrap() http.ResponseWriter {
+// 	return rm.w
+// }
+
+func (rm *ResponseModifier) WriteHeader(code int) {
+	rm.statusCode = code
+}
+
+// BodyReader returns a reader for the response body.
+// Every call to this function will return a new reader that starts from the beginning of the buffer.
+func (rm *ResponseModifier) BodyReader() io.ReadCloser {
+	if rm.buf == nil {
+		return io.NopCloser(bytes.NewReader(nil))
+	}
+	return io.NopCloser(bytes.NewReader(rm.buf.Bytes()))
+}
+
+func (rm *ResponseModifier) ResetBody() {
+	if !rm.bodyModified {
+		return
+	}
+	if rm.buf == nil {
+		return
+	}
+	rm.buf.Reset()
+}
+
+func (rm *ResponseModifier) SetBody(r io.ReadCloser) error {
+	if rm.buf == nil {
+		rm.buf = rm.bufPool.GetBuffer()
+	} else {
+		rm.buf.Reset()
+	}
+
+	rm.bodyModified = true
+
+	_, err := io.Copy(rm.buf, r)
+	if err != nil {
+		return fmt.Errorf("failed to copy body: %w", err)
+	}
+	r.Close()
+	return nil
+}
+
+func (rm *ResponseModifier) ContentLength() int {
+	if !rm.bodyModified {
+		if rm.origContentLength >= 0 {
+			return int(rm.origContentLength)
+		}
+		contentLength, _ := strconv.Atoi(rm.ContentLengthStr())
+		return contentLength
+	}
+	return rm.buf.Len()
+}
+
+func (rm *ResponseModifier) ContentLengthStr() string {
+	if !rm.bodyModified {
+		if rm.origContentLength >= 0 {
+			return strconv.FormatInt(rm.origContentLength, 10)
+		}
+		return rm.w.Header().Get("Content-Length")
+	}
+	return strconv.Itoa(rm.buf.Len())
+}
+
+func (rm *ResponseModifier) Content() []byte {
+	if rm.buf == nil {
+		return nil
+	}
+	return rm.buf.Bytes()
+}
+
+func (rm *ResponseModifier) StatusCode() int {
+	if rm.statusCode == 0 {
+		return http.StatusOK
+	}
+	return rm.statusCode
+}
+
+func (rm *ResponseModifier) Header() http.Header {
+	return rm.w.Header()
+}
+
+func (rm *ResponseModifier) Response() Response {
+	return Response{StatusCode: rm.StatusCode(), Header: rm.Header()}
+}
+
+func (rm *ResponseModifier) Write(b []byte) (int, error) {
+	if len(b) == 0 {
+		return 0, nil
+	}
+
+	rm.bodyModified = true
+	if rm.buf == nil {
+		rm.buf = rm.bufPool.GetBuffer()
+	}
+	return rm.buf.Write(b)
+}
+
+// AppendError appends an error to the response modifier
+func (rm *ResponseModifier) AppendError(format string, args ...any) {
+	rm.errs.Addf(format, args...)
+}
+
+func (rm *ResponseModifier) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if hijacker, ok := rm.w.(http.Hijacker); ok {
+		rm.hijacked = true
+		return hijacker.Hijack()
+	}
+	return nil, nil, errors.New("hijack not supported")
+}
+
+// FlushRelease flushes the response modifier and releases the resources
+// it returns the number of bytes written and the aggregated error
+// if there is any error (rule errors or write error), it will be returned
+func (rm *ResponseModifier) FlushRelease() (int, error) {
+	n := 0
+	if !rm.hijacked {
+		if rm.bodyModified {
+			h := rm.w.Header()
+			h.Set("Content-Length", rm.ContentLengthStr())
+			h.Del("Transfer-Encoding")
+			h.Del("Trailer")
+		}
+		rm.w.WriteHeader(rm.StatusCode())
+
+		if rm.bodyModified {
+			if content := rm.Content(); len(content) > 0 {
+				nn, werr := rm.w.Write(content)
+				n += nn
+				if werr != nil {
+					rm.errs.Addf("write error: %w", werr)
+				}
+				if err := http.NewResponseController(rm.w).Flush(); err != nil && !errors.Is(err, http.ErrNotSupported) {
+					rm.errs.Addf("flush error: %w", err)
+				}
+			}
+		}
+	}
+
+	// release the buffer and reset the pointers
+	if rm.buf != nil {
+		rm.bufPool.PutBuffer(rm.buf)
+		rm.buf = nil
+	}
+
+	// release the shared data
+	if rm.shared != nil {
+		rm.shared.Release()
+		rm.shared = nil
+	}
+
+	return n, rm.errs.Error()
+}
