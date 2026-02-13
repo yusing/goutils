61439ccc9fe4af024f50543a395646de50da2055
yusing <yusing.wys@gmail.com>
2026-02-13 22:06:47 +0800

    feat(events): add UUID generation to Event struct and update NewEvent function

--- events/event.go
+++ events/event.go
@@ -1,20 +1,26 @@
 package events
 
-import "time"
+import (
+	"time"
+
+	strutils "github.com/yusing/goutils/strings"
+)
 
 type Event struct {
+	ID        string    `json:"uuid"`
 	Timestamp time.Time `json:"timestamp"`
 	Level     Level     `json:"level"`
 	Category  string    `json:"category"`
 	Action    string    `json:"action"`
 	Data      any       `json:"data"`
-}
+} // @name Event
 
 func NewEvent(level Level, category, action string, data any) Event {
 	if level == "" {
 		level = LevelInfo
 	}
 	return Event{
+		ID:        strutils.NewUUIDv7(),
 		Timestamp: time.Now(),
 		Level:     level,
 		Category:  category,
