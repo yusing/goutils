yusing/goutils

commit a5d1c07b212d70718ed1760abc68a2a8cbf21561

feat(strings): add UnmarshalYAML to Redacted type
Refactor UnmarshalJSON to use local helper function for consistency
with the new UnmarshalYAML implementation.

strings/redacted.go
@@ -1,7 +1,6 @@
 package strutils
 
 import (
-	"encoding/json"
 	"strings"
 )
 
@@ -28,7 +27,11 @@ func (r Redacted) MarshalYAML() ([]byte, error) {
 }
 
 func (r *Redacted) UnmarshalJSON(data []byte) error {
-	return json.Unmarshal(data, &r)
+	return UnmarshalJSON(data, &r)
+}
+
+func (r *Redacted) UnmarshalYAML(data []byte) error {
+	return UnmarshalYAML(data, &r)
 }
 
 func Redact(s string) string {

commit 79b607d0de5b4916bc0a131c8ff397bec6eaaa33

fix(strings): Redacted Unmarshal stack overflow
- Infinite loop, introduced in a5d1c07b212d70718ed1760abc68a2a8cbf21561

strings/redacted.go
@@ -27,11 +27,23 @@ func (r Redacted) MarshalYAML() ([]byte, error) {
 }
 
 func (r *Redacted) UnmarshalJSON(data []byte) error {
-	return UnmarshalJSON(data, &r)
+	var s string
+	err := UnmarshalJSON(data, &s)
+	if err != nil {
+		return err
+	}
+	*r = Redacted(s)
+	return nil
 }
 
 func (r *Redacted) UnmarshalYAML(data []byte) error {
-	return UnmarshalYAML(data, &r)
+	var s string
+	err := UnmarshalYAML(data, &s)
+	if err != nil {
+		return err
+	}
+	*r = Redacted(s)
+	return nil
 }
 
 func Redact(s string) string {